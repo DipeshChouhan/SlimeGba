54fee833d2274236f43a5ecacb02e7b033d83da5
DipeshChouhan <dcdon077@gmail.com>
2022-10-31 09:18:04 +0530

    cJSON library added

--- src/slime.c
+++ src/slime.c
@@ -3,17 +3,17 @@
 #include "gba/gba.h"
 #include "utils/file_open.h"
 int main(int argc, char **argv) {
-  // if (argc < 2) {
-  //   printf("Please provide GBA rom\n");
-  //   return 0;
-  // }
+  if (argc < 2) {
+    printf("please provide gba rom\n");
+    return 0;
+  }
   unsigned int rom_size = 0;
-  // uint8_t *rom = load_binary_file(argv[1], &rom_size);
-  // if (rom == NULL) {
-  //   printf("Make sure rom is correct\n");
-  //   return 0;
-  // }
+  uint8_t *rom = load_binary_file(argv[1], &rom_size);
+  if (rom == NULL) {
+    printf("Make sure rom is correct\n");
+    return 0;
+  }
   
-  power_on_gba(NULL, rom_size);
+  power_on_gba(rom, rom_size);
   return 0;
 }
--- src/utils/file_open.c
+++ src/utils/file_open.c
@@ -1,43 +1,45 @@
+#include "file_open.h"
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "file_open.h"
+#include <stdlib.h>
 
-uint8_t* load_binary_file(const char* file_path, unsigned int *loaded) {
-    FILE *fp = fopen(file_path, "rb");
+uint8_t *load_binary_file(const char *file_path, unsigned int *loaded) {
+  FILE *fp = fopen(file_path, "rb");
 
-    if (fp == NULL) {
-        perror("fopen()");
-        return NULL;
-    }
+  if (fp == NULL) {
+    perror("fopen()");
+    return NULL;
+  }
 
-    if (fseek(fp, 0, SEEK_END) != 0) {
-        perror("fseek()");
-        return NULL;
-    }
+  if (fseek(fp, 0, SEEK_END) != 0) {
+    perror("fseek()");
+    return NULL;
+  }
 
-    size_t file_size = ftell(fp);
-    if (file_size < 1) { perror("File is empty!");
-        return NULL;
-    }
-    rewind(fp);
+  size_t file_size = ftell(fp);
+  if (file_size < 1) {
+    perror("File is empty!");
+    return NULL;
+  }
+  rewind(fp);
+  
 
-    uint8_t* data = (uint8_t*)malloc(sizeof(uint8_t)*file_size);
+  uint8_t *data = (uint8_t *)malloc(sizeof(uint8_t) * file_size);
 
-    if (data == NULL) {
-        perror("calloc()");
-        return NULL;
-    }
+  if (data == NULL) {
+    perror("calloc()");
+    return NULL;
+  }
 
-    size_t n_reads = fread(data, sizeof(*data), file_size, fp);
-    if (n_reads != file_size){
-        perror("fread()");
-        free(data);
-        return NULL;
-    }
+  size_t n_reads = fread(data, sizeof(*data), file_size, fp);
+  if (n_reads != file_size) {
+    perror("fread()");
+    free(data);
+    return NULL;
+  }
 
-    *loaded = file_size;
-    fclose(fp);
-    return data;
+  *loaded = file_size;
+  fclose(fp);
+  return data;
 }
